7cd18a86d2ecdeedb623cb107f3d745092668569
Tanish2207 <bhamare.tn@gmail.com>
2024-04-28 00:11:39 +0530

    🐛 fix(app): Fix Car animation, add arrangeTD, page2 pages with layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './App.css';
 import { Canvas } from '@react-three/fiber';
 import { PresentationControls, Environment, ScrollControls, Scroll, OrbitControls } from '@react-three/drei'
 import { Aston } from './components/Aston';
-import Navbar from './components/navbar';
 import Landing from './components/landing';
-import Page2 from './components/page2';
-import Page3 from './components/page3';
-import TestDrive from './components/testDrive';
+import Page2 from './components/page2/page2';
+import ArrangeTD from './components/arrangeTD/arrangeTD';
+import Overlay from './overlay';
+
 
 
 
@@ -14,23 +14,19 @@ function App() {
 
   return (
     <>
-      <Canvas dpr={[1, 2]} shadows camera={{ fov: 90 }} style={{ position : "absolute" }}  >
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 50, position: [0,0,0], rotation: [0,0,0]}} style={{ position : "absolute" }}>
 
-        <color attach="background" args={["#35373b"]} />
-        <ambientLight intensity={3} />
+        <color attach="background" args={["#021926"]} />
+        <ambientLight intensity={6} />
         {/* <PresentationControls global> */}
         <OrbitControls enabled={false}/>
           <Environment files="/env/metro_noord_2k.hdr" />
-          <ScrollControls pages={2} damping={0.2}> */
+          <ScrollControls pages={4} damping={0.2}> 
             <Aston scale={1} />
             <Scroll html style={{ width: "100%", margin: "0", padding: "0" }}>
             
-              <Navbar />
-              <Landing />
-              <Page2 />
-              <TestDrive/>
-             </Scroll>
-
+             <Overlay />
+             </ Scroll>
           </ScrollControls>
 
         {/* </PresentationControls> */}
